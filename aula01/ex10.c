a3b175068c1fa7e17a06cd5df2f9eb09e74034f7
Diogo Quintão <quintaodiogo@gmail.com>
2024-08-30 22:55:56 -0300

    Ajustes no exercício 10 da aula1 e no .gitignore

--- aula01/ex10.c
+++ aula01/ex10.c
@@ -1,3 +1,7 @@
+/*Elabore um programa que faça leitura de vários números inteiros,
+até que se digite um número negativo. O programa tem que retornar o maior e o
+menor número lido*/
+
 #include <stdio.h>
 #include <windows.h>
 
